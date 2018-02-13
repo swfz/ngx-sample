d7fdfa271af9ce1439f5eb34f264876cfd7e1df2
swfz <sawafuji.09@gmail.com>
2018-02-13 23:40:20 +0900

    chartjs

--- src/app/pages/chartjs/chartjs.component.ts
+++ src/app/pages/chartjs/chartjs.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ChartjsComponent implements OnInit {
 
   public data: any;
+  public options: any;
   constructor() { }
 
   ngOnInit() {
@@ -16,14 +17,26 @@ export class ChartjsComponent implements OnInit {
       datasets: [
         {
           label: 'First Dataset',
+          borderColor: '#ff3399',
           data: [65, 59, 80, 81, 56, 55, 40]
         },
         {
           label: 'Second Dataset',
+          borderColor: '#99ff33',
+          data: [ 80, 81, 56, 55, 40, 65, 59]
+        },
+        {
+          label: 'Third Dataset',
+          borderColor: '#3399ff',
           data: [28, 48, 40, 19, 86, 27, 90]
         }
       ]
-    }
-  }
+    };
 
+    this.options = {
+      tooltips: {
+        mode: 'index'
+      }
+    };
+  }
 }
