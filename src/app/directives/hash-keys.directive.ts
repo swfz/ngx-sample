53141921285614f14ce5060f30e934ef957b257c
swfz <sawafuji.09@gmail.com>
2018-03-23 05:19:48 +0900

    structual directive

--- src/app/directives/hash-keys.directive.ts
+++ src/app/directives/hash-keys.directive.ts
@@ -11,18 +11,27 @@ import {
   selector: '[appHashKeys]'
 })
 export class HashKeysDirective implements OnChanges {
+  // let item of itemsで ${selector}Ofというinputが渡ってくる
+  // これがハッシュ
   @Input() appHashKeysOf: any;
 
   constructor(
+    // ディレクティブを定義したコンテナ
     private viewContainerRef: ViewContainerRef,
+    // appHashKeysを適用した中身のテンプレート
     private templateRef: TemplateRef<any>
   ) {}
 
   ngOnChanges(changes: SimpleChanges) {
+    // それまで生成されていたDOMを削除
     this.viewContainerRef.clear();
+    // キーを取得
     const keys = Object.keys(this.appHashKeysOf);
     keys.forEach(key => {
-      const view = this.viewContainerRef.createEmbeddedView(this.templateRef, {
+      // キーごとにテンプレートを生成
+      this.viewContainerRef.createEmbeddedView(this.templateRef, {
+        // let item of itemsのitemの箇所に適用される変数が$implicit
+        // ngForOfのようにindex,first,last,even,oddなど独自のテンプレート変数も定義できる
         $implicit: key
       });
     });
