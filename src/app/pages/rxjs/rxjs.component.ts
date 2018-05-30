552724ef1a207a03974bc2971daed8560e779f1a
swfz <sawafuji.09@gmail.com>
2018-05-31 08:09:45 +0900

    rxjs

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit, AfterViewInit, ViewChild} from '@angular/core';
 import {Subject, Observable} from "rxjs";
+import {bufferTime, distinctUntilChanged, filter, pairwise} from "rxjs/operators";
 
+export interface IEvent {
+  type: string;
+}
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
@@ -8,17 +12,34 @@ import {Subject, Observable} from "rxjs";
 })
 export class RxjsComponent implements OnInit {
 
-  public inputText: any;
   public aaa: Observable<number> = new Subject;
-  constructor(
 
+  private events$: Subject<IEvent>;
+  private inputTexts$: Subject<string>;
+  constructor(
   ) { }
 
   ngOnInit(){
     this.aaa.subscribe(result => {
       console.log('observer recieved' + result);
     });
+    this.events$ = new Subject<IEvent>();
 
+    this.events$.pipe(
+      distinctUntilChanged((a: IEvent, b: IEvent) => a.type === b.type),
+      bufferTime(5000),
+      ).subscribe(events => {
+        console.log(events);
+      });
+
+    this.inputTexts$ = new Subject<string>();
+
+    this.inputTexts$.pipe(
+      pairwise(),
+      // filter(p => p[0] === p[1])
+    ).subscribe(p => {
+      console.log(p);
+    });
   }
 
   @ViewChild('input') text;
@@ -67,6 +88,18 @@ export class RxjsComponent implements OnInit {
         () =>  console.log('completed')
       );
   }
+
+  clickEvent(type) {
+    this.events$.next({type: type});
+  }
+
+  keyUpEvent(e) {
+    // console.log(e);
+    // console.log(e.target.value);
+    this.inputTexts$.next(e.target.value);
+  }
+
+
 
   private sleep(ms: number){
     setTimeout(console.log('timeout'),ms);
