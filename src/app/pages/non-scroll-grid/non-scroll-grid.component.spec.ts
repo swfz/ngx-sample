d66da0dcc89ddf97a65fa586acc010176cebcc7d
swfz <sawafuji.09@gmail.com>
2019-04-14 13:56:42 +0900

    Add Component

---
+++ src/app/pages/non-scroll-grid/non-scroll-grid.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NonScrollGridComponent } from './non-scroll-grid.component';
+
+describe('NonScrollGridComponent', () => {
+  let component: NonScrollGridComponent;
+  let fixture: ComponentFixture<NonScrollGridComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NonScrollGridComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NonScrollGridComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/non-scroll-grid/non-scroll-grid.component.ts
@@ -0,0 +1,44 @@
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {ColDef, GridOptions} from "ag-grid";
+
+@Component({
+  selector: 'app-non-scroll-grid',
+  templateUrl: './non-scroll-grid.component.html',
+  styleUrls: ['./non-scroll-grid.component.scss']
+})
+export class NonScrollGridComponent implements OnInit {
+
+  public gridOptions: GridOptions;
+  public columnDefs: ColDef[];
+  public gridRows: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.gridOptions = <GridOptions>{};
+    this.gridRows = this.getDummyGridRows();
+  }
+
+  onGridReady(params) {
+    params.api.setDomLayout('print');
+  }
+
+  private getDummyGridRows() {
+    const categories = ['hoge', 'fuga', 'piyo'];
+
+    return Array.apply(null, { length: 100 })
+      .map(Number.call, Number)
+      .map(n => {
+        const s1 = Math.floor(Math.random() * 1000);
+        const s2 = Math.floor(Math.random() * 100);
+        return {
+          accountId: n,
+          name: `テストアカウント${n}`,
+          category: categories[Math.floor(Math.random() * categories.length)],
+          score1: s1,
+          score2: s2,
+          rate: Math.round((s2 / s1) * 100) / 100
+        };
+      });
+  }
+}
