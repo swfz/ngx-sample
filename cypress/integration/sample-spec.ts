fbe3cd8ea7f826b9263c1a60b2c11a344c453fa1
swfz <sawafuji.09@gmail.com>
2019-04-17 22:21:47 +0900

    apply prettier

--- cypress/integration/sample-spec.ts
+++ cypress/integration/sample-spec.ts
@@ -1,19 +1,19 @@
 describe('My First Test', function() {
   it('Does not do much!', function() {
-    expect(true).to.equal(true)
-  })
+    expect(true).to.equal(true);
+  });
 });
 describe('number input directive', function() {
   const urlBase = 'http://localhost:4200/';
   it('format at focus out', function() {
     cy.visit(`${urlBase}focus`);
 
-    cy.get('[name="directive-input"]').should('have.value', '10,000')
+    cy.get('[name="directive-input"]').should('have.value', '10,000');
     cy.get('[name="directive-input"]').focus();
-    cy.get('[name="directive-input"]').should('have.value', '10000')
+    cy.get('[name="directive-input"]').should('have.value', '10000');
     cy.get('[name="directive-input"]').blur();
-    cy.get('[name="directive-input"]').should('have.value', '10,000')
-  })
+    cy.get('[name="directive-input"]').should('have.value', '10,000');
+  });
 });
 describe('ag-grid flex box', function() {
   const urlBase = 'http://localhost:4200/';
@@ -21,10 +21,20 @@ describe('ag-grid flex box', function() {
   it('collapse and expand', function() {
     cy.visit(`${urlBase}grid/flex`);
 
-    cy.get('div.card-block').contains('Menu2 content').should('be.visible');
-    cy.get('button').contains('Toggle Menu2 collapse').click();
-    cy.get('div.card-block').contains('Menu2 content').should('be.hidden');
-    cy.get('button').contains('Toggle Menu2 collapse').click();
-    cy.get('div.card-block').contains('Menu2 content').should('be.visible');
-  })
+    cy.get('div.card-block')
+      .contains('Menu2 content')
+      .should('be.visible');
+    cy.get('button')
+      .contains('Toggle Menu2 collapse')
+      .click();
+    cy.get('div.card-block')
+      .contains('Menu2 content')
+      .should('be.hidden');
+    cy.get('button')
+      .contains('Toggle Menu2 collapse')
+      .click();
+    cy.get('div.card-block')
+      .contains('Menu2 content')
+      .should('be.visible');
+  });
 });
