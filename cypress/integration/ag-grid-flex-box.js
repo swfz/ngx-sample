dd44b74c5773f410ba65c79f7d6dd4fcbfb3fc7f
swfz <sawafuji.09@gmail.com>
2019-04-25 04:05:13 +0900

    [e2e]case typo

--- cypress/integration/ag-grid-flex-box.js
+++ cypress/integration/ag-grid-flex-box.js
@@ -5,19 +5,19 @@ describe('ag-grid flex box', () => {
     cy.visit(`${urlbase}grid/flex`);
 
     cy.get('div.card-block')
-      .contains('menu2 content')
+      .contains('Menu2 content')
       .should('be.visible');
     cy.get('button')
-      .contains('toggle menu2 collapse')
+      .contains('Toggle Menu2 collapse')
       .click();
     cy.get('div.card-block')
-      .contains('menu2 content')
+      .contains('Menu2 content')
       .should('be.hidden');
     cy.get('button')
-      .contains('toggle menu2 collapse')
+      .contains('Toggle Menu2 collapse')
       .click();
     cy.get('div.card-block')
-      .contains('menu2 content')
+      .contains('Menu2 content')
       .should('be.visible');
 
     cy.compareSnapshot('ag-grid-flexbox', 0.3);
