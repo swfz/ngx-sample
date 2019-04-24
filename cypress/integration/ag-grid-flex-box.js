2f54ac719aa5541bfc7e5f7d41b1504002986d9a
swfz <sawafuji.09@gmail.com>
2019-04-25 03:39:12 +0900

    [e2e] add test

---
+++ cypress/integration/ag-grid-flex-box.js
@@ -0,0 +1,27 @@
+describe('ag-grid flex box', () => {
+  const urlbase = 'http://localhost:4200/';
+
+  it('collapse and expand', () => {
+    cy.visit(`${urlbase}grid/flex`);
+
+    cy.get('div.card-block')
+      .contains('menu2 content')
+      .should('be.visible');
+    cy.get('button')
+      .contains('toggle menu2 collapse')
+      .click();
+    cy.get('div.card-block')
+      .contains('menu2 content')
+      .should('be.hidden');
+    cy.get('button')
+      .contains('toggle menu2 collapse')
+      .click();
+    cy.get('div.card-block')
+      .contains('menu2 content')
+      .should('be.visible');
+
+    cy.comparesnapshot('ag-grid-flexbox', 0.3);
+  });
+});
+
+
--- cypress/integration/sample-spec.js
+++ cypress/integration/sample-spec.js
@@ -3,42 +3,4 @@ describe('My First Test', function() {
     expect(true).to.equal(true);
   });
 });
-describe('number input directive', function() {
-  const urlBase = 'http://localhost:4200/';
-  it('format at focus out', function() {
-    cy.visit(`${urlBase}focus`);
 
-    cy.get('[name="directive-input"]').should('have.value', '10,000');
-    cy.get('[name="directive-input"]').focus();
-    cy.get('[name="directive-input"]').should('have.value', '10000');
-    cy.get('[name="directive-input"]').blur();
-    cy.get('[name="directive-input"]').should('have.value', '10,000');
-
-    cy.compareSnapshot('number-input-form', 0);
-  });
-});
-describe('ag-grid flex box', function() {
-  const urlBase = 'http://localhost:4200/';
-
-  it('collapse and expand', function() {
-    cy.visit(`${urlBase}grid/flex`);
-
-    cy.get('div.card-block')
-      .contains('Menu2 content')
-      .should('be.visible');
-    cy.get('button')
-      .contains('Toggle Menu2 collapse')
-      .click();
-    cy.get('div.card-block')
-      .contains('Menu2 content')
-      .should('be.hidden');
-    cy.get('button')
-      .contains('Toggle Menu2 collapse')
-      .click();
-    cy.get('div.card-block')
-      .contains('Menu2 content')
-      .should('be.visible');
-
-    cy.compareSnapshot('ag-grid-flexbox', 0.3);
-  });
-});
