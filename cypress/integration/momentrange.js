3f3f8abf2df8c56fa341655c573003e14264da08
swfz <sawafuji.09@gmail.com>
2019-04-25 20:14:59 +0900

    [e2e] momentrange

---
+++ cypress/integration/momentrange.js
@@ -0,0 +1,13 @@
+describe('momentrange', () => {
+  const urlBase = 'http://localhost:4200/';
+
+  it('is-visible', () => {
+    cy.visit(`${urlBase}momentrange`);
+
+    // 画面が見れるか
+    cy.contains('true').should('is.visible');
+    cy.contains('false').should('is.visible');
+
+    cy.compareSnapshot('moment-range-is-visible', 0.01);
+  });
+});
