81637add54f678a4d449c84ca8f80ec13b766ad2
swfz <sawafuji.09@gmail.com>
2019-05-27 21:38:39 +0900

    debug shell

--- docker/jq/kick_circle.sh
+++ docker/jq/kick_circle.sh
@@ -2,4 +2,7 @@
 
 branch=$(cat /github/workflow/event.json | jq '.pull_request.head.ref')
 
+echo $branch
+echo $HOGE_TOKEN
+
 curl -v -u $HOGE_TOKEN: -X POST -d \'{"branch": "${branch}"}\' "https://circleci.com/api/v1.1/project/github/swfz/ngx-sample/build"
