8081164fe19ec92708227b54158ed11c66e2c347
swfz <sawafuji.09@gmail.com>
2019-05-30 04:38:27 +0900

    fix ブランチ指定ができてなかったので修正

--- docker/jq/kick_circle.sh
+++ docker/jq/kick_circle.sh
@@ -1,8 +1,10 @@
-#!/bin/sh -x
+#!/bin/sh
 
 branch=$(cat /github/workflow/event.json | jq '.pull_request.head.ref')
 
-echo $branch
-echo $HOGE_TOKEN
+curl -u $CIRCLE_TOKEN: \
+     -X POST \
+     --header "Content-Type: application/json" \
+     -d "{\"branch\": \"${branch}\"}" \
+     "https://circleci.com/api/v1.1/project/github/swfz/ngx-sample/build"
 
-curl -v -u $CIRCLE_TOKEN: -X POST -d \'{"branch": "${branch}"}\' "https://circleci.com/api/v1.1/project/github/swfz/ngx-sample/build"
