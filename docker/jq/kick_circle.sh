1f18beb21de07d10f541919927f72e0186d37750
swfz <sawafuji.09@gmail.com>
2019-05-30 10:20:47 +0900

    add parameter revision

--- docker/jq/kick_circle.sh
+++ docker/jq/kick_circle.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 branch=$(cat /github/workflow/event.json | jq '.pull_request.head.ref')
+revision=$(cat /github/workflow/event.json | jq '.pull_request.head.sha')
 
 curl -u $CIRCLE_TOKEN: \
      -X POST \
      --header "Content-Type: application/json" \
-     -d "{\"branch\": \"${branch}\"}" \
+     -d "{\"branch\": \"${branch}\", \"revision\": \"${revision}\"}" \
      "https://circleci.com/api/v1.1/project/github/swfz/ngx-sample/build"
 
