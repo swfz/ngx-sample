f38b62e9ecea5cf223bdceb967365610827cfe83
swfz <sawafuji.09@gmail.com>
2019-05-30 10:55:20 +0900

    branch=master case

--- docker/jq/kick_circle.sh
+++ docker/jq/kick_circle.sh
@@ -2,14 +2,21 @@
 
 branch=$(cat /github/workflow/event.json | jq '.ref' | sed 's/refs\/heads\///')
 revision=$(cat /github/workflow/event.json | jq '.head_commit.id')
+
 echo '--'
 echo $branch
 echo $revision
 echo '--'
 
-curl -q -u $CIRCLE_TOKEN: \
-     -X POST \
-     --header "Content-Type: application/json" \
-     -d "{\"branch\": ${branch},\"revision\": ${revision}}" \
-     "https://circleci.com/api/v1.1/project/github/swfz/ngx-sample/build"
+
+if [ $branch = 'master' ]; then
+  echo 'branch is master. not need kick pipeline.'
+else
+  curl -q -u $CIRCLE_TOKEN: \
+       -X POST \
+       --header "Content-Type: application/json" \
+       -d "{\"branch\": ${branch},\"revision\": ${revision}}" \
+       "https://circleci.com/api/v1.1/project/github/swfz/ngx-sample/build"
+fi
+
 
