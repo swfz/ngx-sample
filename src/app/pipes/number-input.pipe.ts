6364501607bcca37f89595c741ef81cf4dfee99b
swfz <sawafuji.09@gmail.com>
2019-06-15 17:08:35 +0900

    follow test

--- src/app/pipes/number-input.pipe.ts
+++ src/app/pipes/number-input.pipe.ts
@@ -1,16 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
+import * as _ from 'lodash';
 
 @Pipe({
   name: 'numberInput'
 })
 export class NumberInputPipe implements PipeTransform {
-  transform(value: string, digits?: string): string | null {
-    if (digits) {
-      return new DecimalPipe('en').transform(value, digits);
-    } else {
-      return new DecimalPipe('en').transform(value, '1.0-0');
+  transform(value: string | number, digits?: string): string {
+    if (_.isNumber(value)) {
+      if (digits) {
+        return new DecimalPipe('en').transform(value, digits) || '';
+      } else {
+        return new DecimalPipe('en').transform(value, '1.0-0') || '';
+      }
     }
+    return '';
   }
 
   parse(value: string): string {
