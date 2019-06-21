8b80deed262330144ad97429312da8f0fdf07ed6
swfz <sawafuji.09@gmail.com>
2019-06-22 03:26:50 +0900

    update test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,11 +16,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
+  // it(`should have as title 'app'`, async(() => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual('app');
+  // }));
 
   // it('should render title in a h1 tag', async(() => {
   //   const fixture = TestBed.createComponent(AppComponent);
