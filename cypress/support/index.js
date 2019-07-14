f66b21635bf0e399a5d26918dca1dc73340cb73e
swfz <sawafuji.09@gmail.com>
2019-07-14 14:19:30 +0900

    contextにspecファイル名を追加

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,19 +14,18 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-require('cypress-xpath');
+require("cypress-xpath");
 
-// import addContext from 'mochawesome/addContext';
-// Cypress.on('test:after:run', (test, runnable) => {
-//   console.log('test:after:run');
-//   console.log(runnable);
-//   addContext({test}, {value: 'addContext!!!!!!!!!!!!!!'});
-//   if(test.stat === 'failed'){
-//     addContext({test}, `${runnable.parent.title}`);
-//   }
-// });
-
+import addContext from "mochawesome/addContext";
+Cypress.on("test:after:run", (test, runnable) => {
+  addContext({ test }, {
+    title: 'filename',
+    value: {
+      filename: Cypress.spec.name
+    }
+  });
+});
