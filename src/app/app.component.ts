165451862f1fe3b5f385fce7033ca74d35f25d5e
swfz <sawafuji.09@gmail.com>
2019-09-19 19:41:29 +0900

    @types/gtag.jsを追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { environment } from '../environments/environment';
 import { DOCUMENT } from '@angular/common';
 import { NavigationEnd, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { ApiSampleService } from './services/api-sample.service';
 
-declare const gtag: Function;
-
-type WindowWithDataLayer = Window & { dataLayer: unknown[] };
+type Gtag = typeof gtag;
+type WindowWithGtag = Window & { gtag: Gtag };
 
 @Component({
   selector: 'app-root',
@@ -17,35 +17,44 @@ export class AppComponent implements OnInit {
   public isDev: boolean;
   public gaCode: string;
 
-  private windowWithDataLayer!: WindowWithDataLayer;
+  private windowWithGtag!: WindowWithGtag;
   private navigationEndSubscription!: Subscription;
 
   constructor(
     private renderer: Renderer2,
     private router: Router,
+    private apiSampleService: ApiSampleService,
     @Inject(DOCUMENT) private document: Document
   ) {
     this.isDev = !environment.production;
     this.gaCode = environment.gaCode;
-    this.windowWithDataLayer = window as WindowWithDataLayer;
+    this.windowWithGtag = window as WindowWithGtag;
   }
 
   ngOnInit() {
+    // this.apiSampleService.fetchUsers();
+    // const users = await this.apiSampleService.users$.toPromise();
+    // console.log(users);
+    // console.log(Math.floor(Math.random() * users.length));
+    // const user = users[Math.floor(Math.random() * Math.floor(users.length))];
+    // console.log(user);
     this.navigationEndSubscription = this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         console.log(event);
-        gtag('config', environment.gaCode, { page_path: event.url });
+        this.windowWithGtag.gtag('config', environment.gaCode, {
+          page_path: event.url
+        });
+        this.windowWithGtag.gtag('set', { user_id: `'2'` });
       }
     });
-    this.windowWithDataLayer.dataLayer = [];
     const s1 = this.renderer.createElement('script');
     s1.type = 'text/javascript';
-    s1.src = 'https://www.googletagmanager.com/gtag/js?id=UA-73628530-3';
+    s1.src = `https://www.googletagmanager.com/gtag/js?id=${environment.gaCode}`;
     this.renderer.appendChild(this.document.head, s1);
 
     const s2 = this.renderer.createElement('script');
     s2.type = 'text/javascript';
-    s2.text = `function gtag(){dataLayer.push(arguments)};gtag('js', new Date());gtag('config', '${this.gaCode}');`;
+    s2.text = `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)};gtag('js', new Date());gtag('config', '${this.gaCode}');`;
     this.renderer.appendChild(this.document.head, s2);
   }
 }
